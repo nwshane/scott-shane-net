8d50835f688af96e766576ba77d2c714dfcba8fb
Nathan Shane <nathan.shane@flocabulary.com>
2019-02-09 12:36:11 -0500

    Initialized next project and added contact page

---
+++ pages/contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "../components/head";
+
+export default () => {
+  return (
+    <article>
+      <Head
+        title="Contact"
+        description="Contact Scott Shane, author of Objective Troy and national security reporter for The New York Times."
+      />
+      <h1 className="heading is-hidden">Contact Scott Shane</h1>
+
+      <p>
+        To contact Scott Shane, send an email to scott.shane AT nytimes.com.
+      </p>
+
+      <p>
+        To send an encrypted message,{" "}
+        <a
+          href="http://pgp.mit.edu/pks/lookup?op=get&search=0xBC220785DDDE5F70"
+          target="blank"
+        >
+          use this public key
+        </a>
+        .
+      </p>
+    </article>
+  );
+};
