35a75bdbd4bd46b43c475cfb8c1fb5e27ec61d39
Nathan Shane <nathan.shane@flocabulary.com>
2019-02-10 12:14:47 -0500

    Use hooks to simplify code

--- pages/media.js
+++ pages/media.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getJSON } from "jquery";
 import moment from "moment";
 
 import Head from "../components/head";
 
-export default class MediaPage extends React.Component {
-  state = {
-    mediaItems: []
-  };
+export default () => {
+  const [mediaItems, setMediaItems] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     const spreadsheetId = "1iOliJOKefnIxrlmMkvmgspcdcQ4aR0fWwQhI-IC-1AU";
 
     getJSON(
       `http://spreadsheets.google.com/feeds/list/${spreadsheetId}/od6/public/values?alt=json`,
       data => {
-        this.setState({
-          mediaItems: data.feed.entry.map(entryItem => {
+        setMediaItems(
+          data.feed.entry.map(entryItem => {
             const gsxKeys = Object.keys(entryItem).filter(key =>
               key.includes("gsx$")
             );
@@ -26,47 +24,44 @@ export default class MediaPage extends React.Component {
               return mediaItem;
             }, {});
           })
-        });
+        );
       }
     );
-  }
+  }, []);
 
-  render() {
-    const { mediaItems } = this.state;
-    return (
+  return (
+    <div>
+      <Head
+        title="Media"
+        description="Interviews, book talks, and more with Scott Shane, national security reporter for The New York Times and author of Objective Troy."
+      />
+
+      <h1 className="primary-header">
+        Media for <span className="book-title">Objective Troy</span>
+      </h1>
+
       <div>
-        <Head
-          title="Media"
-          description="Interviews, book talks, and more with Scott Shane, national security reporter for The New York Times and author of Objective Troy."
-        />
+        {mediaItems.map(mediaItem => (
+          <section className="media-item" key={mediaItem.title}>
+            <h2>
+              <a href={mediaItem.link} target="_blank">
+                {mediaItem.title}
+              </a>
+            </h2>
 
-        <h1 className="primary-header">
-          Media for <span className="book-title">Objective Troy</span>
-        </h1>
+            <p className="media-item-publication-date">
+              {moment(mediaItem.date, "MM-DD-YYYY").format("MMMM D, YYYY")}
+            </p>
 
-        <div>
-          {mediaItems.map(mediaItem => (
-            <section className="media-item" key={mediaItem.title}>
-              <h2>
-                <a href={mediaItem.link} target="_blank">
-                  {mediaItem.title}
-                </a>
-              </h2>
-
-              <p className="media-item-publication-date">
-                {moment(mediaItem.date, "MM-DD-YYYY").format("MMMM D, YYYY")}
-              </p>
-
-              {mediaItem.description && (
-                <div>
-                  {/* todo: output html? */}
-                  {mediaItem.description}
-                </div>
-              )}
-            </section>
-          ))}
-        </div>
+            {mediaItem.description && (
+              <div>
+                {/* todo: output html? */}
+                {mediaItem.description}
+              </div>
+            )}
+          </section>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
