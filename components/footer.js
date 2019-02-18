61e43c73cd714a97d94aa80ec66ce90f9176e680
Nathan Shane <nathan.shane@flocabulary.com>
2019-02-18 09:47:07 -0500

    Fix className

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
 export default () => (
-  <footer class="app-footer">
-    <div class="app-footer-acknowledgments">
-      <p class="app-footer-acknowledgments-item mod-banner-photo">
+  <footer className="app-footer">
+    <div className="app-footer-acknowledgments">
+      <p className="app-footer-acknowledgments-item mod-banner-photo">
         Banner photo &copy; Parker Knight 2008
       </p>
-      <p class="app-footer-acknowledgments-item mod-scottshane">
+      <p className="app-footer-acknowledgments-item mod-scottshane">
         scottshane.net &copy; Scott Shane {new Date().getFullYear()}
       </p>
     </div>
